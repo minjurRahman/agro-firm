f81202c6d01a01f4dd50aa2db7e2ef7a44dead0d
Minjur <minjur.rahaman99@gmail.com>
2024-06-04 14:15:08 +0600

    Carosel of Premium product

---
+++ src/components/home/DisplayPrimiumProduct.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+const DisplayPrimiumProduct = () => {
+  return (
+    <div>
+      <h1 className="text-3xl text-center py-6 font-bold">Our Premium Item</h1>
+      <div className="carousel carousel-center rounded-box h-80">
+        <div className="carousel-item">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRB378XAwjCzJVWPTdqK96qNIva_SKrCOuyZVmpN7X-IBYRfwvaeUFNQ91bbEV1ArJ0ngA&usqp=CAU"
+            alt="Pizza"
+          />
+        </div>
+        <div className="carousel-item">
+          <img
+            src="https://www.lightcastlebd.com/wp-content/uploads/2023/05/cow-saving-beef-industry-bangladesh.webp"
+            alt="Pizza"
+          />
+        </div>
+        <div className="carousel-item">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4RX45nOTZXqloL4gTnERRciN8oVr0c3BC-Q&s"
+            alt="Pizza"
+          />
+        </div>
+        <div className="carousel-item">
+          <img
+            src="https://i.ytimg.com/vi/M7ECnieDZc4/sddefault.jpg"
+            alt="Pizza"
+          />
+        </div>
+        <div className="carousel-item">
+          <img
+            src="https://i0.wp.com/farmerstrend.co.ke/wp-content/uploads/2022/08/beef-cattle-brahman-farming-cow-in-kenya.jpg"
+            alt="Pizza"
+          />
+        </div>
+        <div className="carousel-item">
+          <img
+            src="https://facts.net/wp-content/uploads/2023/12/18-beef-cattle-facts-1702297152.jpeg"
+            alt="Pizza"
+          />
+        </div>
+        <div className="carousel-item">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4RX45nOTZXqloL4gTnERRciN8oVr0c3BC-Q&s"
+            alt="Pizza"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DisplayPrimiumProduct;
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Banner from "./Banner";
 import { useLoaderData } from "react-router-dom";
 import CowProduct from "./CowProduct";
+import Accordion from "./Accordion";
+import DisplayPrimiumProduct from "./DisplayPrimiumProduct";
 
 const Home = () => {
   const data = useLoaderData();
@@ -10,6 +12,8 @@ const Home = () => {
     <div>
       <Banner />
       <CowProduct data={data} />
+      <DisplayPrimiumProduct />
+      <Accordion />
     </div>
   );
 };
